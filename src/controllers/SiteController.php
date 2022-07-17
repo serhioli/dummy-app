dd4591f408f46e3f9338a396020ecbb6bb9222d9
Sergei Lysogor <me@serhio.li>
2022-07-17 20:49:23 +0000

    WIP

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -10,7 +10,7 @@ class SiteController extends Controller
     {
         return [
             'error' => [
-                'class' => 'yii\web\ErrorAction',
+                'class' => \yii\web\ErrorAction::class,
             ],
         ];
     }
